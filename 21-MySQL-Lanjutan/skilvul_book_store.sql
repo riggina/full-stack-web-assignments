72cd7f389cddab42b3d4919f15abdf29c8098015
riggina <oceaniariggina@gmail.com>
2021-12-09 02:06:33 +0800

    doneee sql-lanjutan

--- 21-MySQL-Lanjutan/skilvul_book_store.sql
+++ 21-MySQL-Lanjutan/skilvul_book_store.sql
@@ -11,9 +11,8 @@ CREATE TABLE penerbit(
 );
 
 INSERT INTO penerbit(nama, kota) VALUES
-('',''),
-('',''),
-('','');
+('Elex Media Komputindo','Jakarta'),
+('Anak Hebat Indonesia','Jakarta');
 
 CREATE TABLE penulis(
     id INT(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -22,9 +21,9 @@ CREATE TABLE penulis(
 );
 
 INSERT INTO penulis(nama, kota) VALUES
-('',''),
-('',''),
-('','');
+('Ir.Yuniar Supardi','Jawa Barat'),
+('Jubilee Enterprise','Jakarta'),
+('Adam Saputra, S.Si.','Jakarta Selatan');
 
 CREATE TABLE buku(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -39,7 +38,11 @@ CREATE TABLE buku(
 );
 
 INSERT INTO buku VALUES
-('',)
+('', '9786230019760', 'Semua Bisa Menjadi Programmer Javascript & Node.js', 1, 1, 60000, 9),
+('', '9786020422411', 'Otodidak Pemrograman Javascript', 2, 1, 49500, 7),
+('', '9786232443723', 'Buku Sakti HTML, CSS & Javascript', 3, 2, 39600, 8);
+
+
 
 -- Inner Join
 SELECT buku.id, isbn, judul, penulis, penerbit.nama AS penerbit, harga, stock FROM buku INNER JOIN penerbit ON buku.penerbit = penerbit.id;
