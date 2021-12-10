86e21a11e5a847c4e6d6891d905020c4c477c4d8
riggina <oceaniariggina@gmail.com>
2021-12-10 23:46:15 +0800

    update

---
+++ 23-Intro-and-Essential-Nodejs/web-server.js
@@ -0,0 +1,11 @@
+const http = require('http')
+const url = require('url')
+
+const port = 8000
+
+http.createServer((req,res) => {
+    res.writeHead(200, {'Content-Type' : 'text/html'})
+
+    
+
+})
