ef2a3d6b9daade9f68c3d6f2822785ef74d4f3cc
riggina <oceaniariggina@gmail.com>
2021-12-16 02:06:10 +0800

    doneeee

--- 23-Intro-and-Essential-Nodejs/web-server.js
+++ 23-Intro-and-Essential-Nodejs/web-server.js
@@ -3,9 +3,25 @@ const url = require('url')
 
 const port = 8000
 
-http.createServer((req,res) => {
+const server = http.createServer((req,res) => {
+    let pathname = url.parse(req.url).pathname
+
     res.writeHead(200, {'Content-Type' : 'text/html'})
 
-    
-
+    if (pathname === '/about'){
+        res.write('<h1>Welcome to about us page</h1>')
+        res.end()
+    } else if (pathname === '/contact'){
+        res.write('<h1>Welcome to contact us page</h1>')
+        res.end()
+    } else {
+        res.write('<h1>Hello World!</h1>')
+        res.end()
+    }
 })
+
+server.on('listening', () => {
+    console.log("server listening on port", port);
+})
+
+server.listen(port);
